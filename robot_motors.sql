a9353eb7db079364ceb80c20575309ed27e21960
kindaYaz <87319592+kindaYaz@users.noreply.github.com>
2021-07-12 17:30:17 +0300

    Add files via upload

---
+++ robot_motors.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 12, 2021 at 04:29 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 7.3.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `robot`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `robot_motors`
+--
+
+CREATE TABLE `robot_motors` (
+  `Motor1` int(11) NOT NULL,
+  `Motor2` int(11) NOT NULL,
+  `Motor3` int(11) NOT NULL,
+  `Motor4` int(11) NOT NULL,
+  `Motor5` int(11) NOT NULL,
+  `Motor6` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
